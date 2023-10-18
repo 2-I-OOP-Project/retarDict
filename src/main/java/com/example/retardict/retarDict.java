3494ba3171c375e73f90a72985d973cf9bbf2b65
2uan <22028250@vnu.edu.vn>
2023-10-19 00:54:52 +0700

    change gioi tinh DMQuan again

--- src/main/java/com/example/retardict/retarDict.java
+++ src/main/java/com/example/retardict/retarDict.java
@@ -12,7 +12,7 @@ public class retarDict extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(retarDict.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("DMQuanSieuGay");
+        stage.setTitle("DMQuanSieuSieuGay");
         stage.setScene(scene);
         stage.show();
     }
