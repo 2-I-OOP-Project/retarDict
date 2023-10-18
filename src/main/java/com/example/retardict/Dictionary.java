4f96833791aa4d388277b530b7487d31ce41230a
GoatYogurt <minhquan161004@gmail.com>
2023-10-18 21:01:41 +0700

    added new files

---
+++ src/main/java/com/example/retardict/Dictionary.java
@@ -0,0 +1,26 @@
+package com.example.retardict;
+
+import java.util.ArrayList;
+
+public class Dictionary {
+    public final ArrayList<Word> words = new ArrayList<Word>();
+
+    public void insertWord(String target, String explain) {
+        if (target == null) target = "something";
+        if (explain == null) explain = "something aswell";
+        words.add(new Word(target, explain));
+    }
+
+    public void sortByWordTarget() {
+        words.sort(((o1, o2) ->
+                o1.getWord_target().compareTo(o2.getWord_target())));
+    }
+
+    public int getWordCount () {
+        return words.size();
+    }
+
+    public Word getWord(int index) {
+        return words.get(index);
+    }
+}
