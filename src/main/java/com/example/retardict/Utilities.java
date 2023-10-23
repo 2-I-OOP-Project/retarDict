a9155871182eaff5e7942b0392829a5c72038a69
2uan <22028250@vnu.edu.vn>
2023-10-23 23:16:10 +0700

    -added load words to db function to Utilities.java -put some testing data into testdb.db -test.txt for testing

--- src/main/java/com/example/retardict/Utilities.java
+++ src/main/java/com/example/retardict/Utilities.java
@@ -8,11 +8,12 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Utilities {
-    public static void loadFromTextFile(String filePath) {
+    public static List<String> loadFromTextFile(String filePath) {
+        List<String> words = null;
         try {
             File myObj = new File(filePath);
             Scanner myReader = new Scanner(myObj);
-            List<String> words = myReader.useDelimiter("@").tokens().collect(Collectors.toList());
+            words = myReader.useDelimiter("@").tokens().collect(Collectors.toList());
 
 //            words.forEach(System.out::println);
 
@@ -30,23 +31,49 @@ public class Utilities {
             System.out.println("An error occurred.");
             System.out.println(e.getMessage());
         }
+        return words;
     }
 
-    public static void main(String[] args) {
-//        Utilities.loadFromTextFile("src/main/resources/anhviet109K.txt");
-
+    public static void loadToDatabase(List<String> words) {
         try {
             Connection connection = DriverManager.getConnection("jdbc:sqlite:D:/intelliJ/DictionaryMaybe/src/main/resources/testdb.db");
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("select * from test");
-            while(rs.next())
-            {
-                // read the result set
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("surname = " + rs.getString("surname"));
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO words VALUES (?, ?, ?);");
+
+            for (String word : words) {
+                String[] wholeWord = word.split("\n",2);
+                String[] firstLine = wholeWord[0].split("/",2);
+
+                String word_target = firstLine[0];
+                String pronunciation = "/" + firstLine[1];
+                String description = wholeWord[1];
+                statement.setString(1, word_target);
+                statement.setString(2, pronunciation);
+                statement.setString(3, description);
+                statement.executeUpdate();
             }
+
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
+
+    }
+
+    public static void main(String[] args) {
+        List<String> words = Utilities.loadFromTextFile("src/main/resources/test.txt");
+        Utilities.loadToDatabase(words);
+
+
+//        try {
+//            Connection connection = DriverManager.getConnection("jdbc:sqlite:D:/intelliJ/DictionaryMaybe/src/main/resources/testdb.db");
+//            Statement statement = connection.createStatement();
+//            ResultSet rs = statement.executeQuery("select * from test");
+//            while(rs.next()) {
+//                // read the result set
+//                System.out.println("name = " + rs.getString("name"));
+//                System.out.println("surname = " + rs.getString("surname"));
+//            }
+//        } catch (SQLException e) {
+//            System.err.println(e.getMessage());
+//        }
     }
 }
