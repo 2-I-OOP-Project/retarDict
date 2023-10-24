65e7a084c42e6b84d806731d1154804b091d2908
2uan <22028250@vnu.edu.vn>
2023-10-24 16:07:58 +0700

    test changing to add word scene

--- src/main/java/com/example/retardict/Controller.java
+++ src/main/java/com/example/retardict/Controller.java
@@ -1,10 +1,22 @@
 package com.example.retardict;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     @FXML
     private Label welcomeText;
 
@@ -16,5 +28,25 @@ public class Controller {
     @FXML
     protected void bButton() {
         welcomeText.setText("Of course you are!");
+    }
+
+    @FXML
+    public void switchToAddWordScene(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(retarDict.class.getResource("addNewWordScene.fxml"));
+        root = fxmlLoader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void switchBackToMainScene(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(retarDict.class.getResource("hello-view.fxml"));
+        root = fxmlLoader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
