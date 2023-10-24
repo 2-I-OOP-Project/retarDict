0cb81b506a2ca3411b48fa285a6dc32ba08bccca
GoatYogurt <minhquan161004@gmail.com>
2023-10-24 22:27:44 +0700

    small changes

--- src/main/java/com/example/retardict/Controller.java
+++ src/main/java/com/example/retardict/Controller.java
@@ -1,20 +1,36 @@
 package com.example.retardict;
 
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class Controller {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void aButton() {
-        welcomeText.setText("You must be gay!");
+    private TextField searchBox;
+
+    @FXML
+    private ListView<Integer> listView = new ListView<>();
+
+    @FXML
+    protected void getTextInSearchBox() {
+        welcomeText.setText(this.searchBox.getText());
     }
 
     @FXML
-    protected void bButton() {
-        welcomeText.setText("Of course you are!");
+    protected void testFunction() {
+        return;
     }
 }
