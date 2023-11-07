cab387e14db8e7f665bd39969da07133b6f904d2
2uan <22028250@vnu.edu.vn>
2023-11-07 23:34:20 +0700

    - added common sidePane for all scene - change old controller to use initialize with side pane

--- src/main/java/com/example/retardict/GameSceneController.java
+++ src/main/java/com/example/retardict/GameSceneController.java
@@ -3,17 +3,35 @@ package com.example.retardict;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class GameSceneController {
+public class GameSceneController implements Initializable {
     private Parent root;
     private Stage stage;
     private Scene scene;
+
+    @FXML
+    private AnchorPane rootAnchor;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        FXMLLoader sidePaneLoader = new FXMLLoader(getClass().getResource("SidePane.fxml"));
+        try {
+            Parent sidePaneLoaded = sidePaneLoader.load();
+            rootAnchor.getChildren().addAll(sidePaneLoaded);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @FXML
     public void switchToMultipleChoiceScene(ActionEvent event) throws IOException {
@@ -39,4 +57,5 @@ public class GameSceneController {
         stage.setScene(scene);
         stage.show();
     }
+
 }
